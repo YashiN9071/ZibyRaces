af0bbf4f0ec91a476a13dcf675a785d322d94ec2
NickYashin <nik.yashin.03@inbox.ru>
2022-01-08 10:11:02 +0300

    player move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-from image_load import load_image
+# from image_load import load_image
 
 pygame.init()
 
@@ -7,6 +7,7 @@ pygame.init()
 # player
 class PlayerCar(pygame.sprite.Sprite):
     def __init__(self):
+        self.for_move = 2
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface((100, 200))
         self.image.fill(pygame.Color('Red'))
@@ -14,7 +15,12 @@ class PlayerCar(pygame.sprite.Sprite):
         self.rect.center = (WIDTH // 2, 880)
 
     def move(self, key):
-        self.rect.move(10, 0)
+        if key == pygame.K_a and self.for_move - 1 > 0:
+            self.rect.left -= 300
+            self.for_move -= 1
+        elif key == pygame.K_d and self.for_move + 1 < 4:
+            self.rect.left += 300
+            self.for_move += 1
 
 
 # game
@@ -23,18 +29,20 @@ screen = pygame.display.set_mode(size)
 
 all_sprites = pygame.sprite.Group()
 player_sprite = pygame.sprite.Group()
+obstacle_sprite = pygame.sprite.Group()
+not_an_object_sprite = pygame.sprite.Group()
 
 player = PlayerCar()
 player_sprite.add(player)
 
-fps = 30
+fps = 60
 clock = pygame.time.Clock()
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-        while event.type == pygame.KEYDOWN:
+        if event.type == pygame.KEYDOWN:
             player.move(event.key)
     screen.fill(pygame.Color("black"))
     player_sprite.update()
