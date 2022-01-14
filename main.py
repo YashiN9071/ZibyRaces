cf91d96cbdc52a53df6acec128b92391a83c3b02
NickYashin <nik.yashin.03@inbox.ru>
2022-01-15 00:11:46 +0300

    15.01.2022 0:11

--- main.py
+++ main.py
@@ -7,6 +7,18 @@ from image_load import load_image
 
 pygame.init()
 
+size = WIDTH, HEIGHT = 1920, 1080
+screen = pygame.display.set_mode(size)
+
+fps = 60
+clock = pygame.time.Clock()
+
+
+# menu class
+class Menu:
+    def __init__(self):
+        pass
+
 
 # player
 class PlayerCar(pygame.sprite.Sprite):
@@ -59,7 +71,7 @@ class ObstacleCarFromYou(pygame.sprite.Sprite):
         self.rect.center = (place, -100)
 
     def update(self):
-        self.rect.top += 1
+        self.rect.top += 2
         if self.rect.y > 1100:
             self.kill()
 
@@ -82,7 +94,7 @@ class ObstacleCarOnYou(pygame.sprite.Sprite):
 class BigObstacle(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__(obstacles_sprite)
-        self.image = pygame.Surface((280, 280))
+        self.image = pygame.Surface((300, 300))
         self.image.fill(pygame.Color('White'))
         self.rect = self.image.get_rect()
         place = random.randrange(660, 1261, 300)
@@ -132,19 +144,67 @@ def random_obstacle():
         SmallObstacle()
 
 
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+
+def start_screen():
+    intro_text = ['Ziby Races', ' ', 'press any key to continue']
+
+    fon = pygame.transform.scale(pygame.Surface(size), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    text_coord = 0
+    f = pygame.font.SysFont('arial', 250)
+    for line in intro_text:
+        string_rendered = f.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        f = pygame.font.SysFont('arial', 30)
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.center = (WIDTH // 2, HEIGHT // 2 + text_coord)
+        text_coord += 200
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(fps)
+
+
+def set_difficulty(value, difficulty):
+    pass
+
+
+def start_the_game():
+    return False
+
+
 # game
+font = pygame.font.SysFont('arial', 50)
+
+player_sprite = pygame.sprite.Group()
+obstacles_sprite = pygame.sprite.Group()
+not_an_object_sprite = pygame.sprite.Group()
+
+start_screen()
+
 run = True
 while run:
-    size = WIDTH, HEIGHT = 1920, 1080
-    screen = pygame.display.set_mode(size)
-
-    player_sprite = pygame.sprite.Group()
-    obstacles_sprite = pygame.sprite.Group()
-    not_an_object_sprite = pygame.sprite.Group()
-
-    font = pygame.font.SysFont('arial', 50)
-
     # menu
+    running = True
+    while running:
+        menu = pygame_menu.Menu('Welcome', 400, 300, theme=pygame_menu.themes.THEME_BLUE)
+
+        menu.add.text_input('Name :', default='John Doe')
+        menu.add.selector('Difficulty :', [('Hard', 1), ('Easy', 2)], onchange=set_difficulty)
+        menu.add.button('Play', start_the_game)
+        menu.add.button('Quit', pygame_menu.events.EXIT)
 
     # environment
     for pos in range(510, 1411, 300):
@@ -158,10 +218,9 @@ while run:
     player = PlayerCar()
     player_sprite.add(player)
 
-    fps = 60
     score = 0
     tps = 200
-    clock = pygame.time.Clock()
+
     running = True
     while running:
         # buttons
@@ -170,11 +229,19 @@ while run:
                 running = False
             if event.type == pygame.KEYDOWN:
                 player.move(event.key)
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_p:
+                    score += 100000
 
         # create obstacles
         if tps >= 250:
             random_obstacle()
             tps = 0
+
+        # check score
+        if score >= 100000:
+            running = False
+            run = False
 
         # check intersection
         player_rect = player.rect_check()
@@ -182,7 +249,7 @@ while run:
 
         # screen update
         screen.fill(pygame.Color("black"))
-        score_text = font.render(str(score), 5, (255, 255, 255))
+        score_text = font.render('Your score: ' + str(score), 5, (255, 255, 255))
         screen.blit(score_text, (20, 10))
         not_an_object_sprite.update()
         obstacles_sprite.update()
